4da800fa8e4008d2d7f13b297776290860d34de7
Connor Kerns <connor-k@users.noreply.github.com>
2016-02-27 14:53:00 -0800

    Display table for query

--- hw6/e4684c9edbba6535ee251c0b32c402e8.php
+++ hw6/e4684c9edbba6535ee251c0b32c402e8.php
@@ -33,6 +33,32 @@
             margin-right: 65px;
             margin-bottom: 24px;
         }
+        
+        th {
+            background-color: #F2F2F2;
+        }
+        
+        .response_table {
+            width: 640px;
+            margin: 8px auto;
+            border-collapse: collapse;
+            border-style: solid;
+            border-width: 2px;
+            border-color: #C7C7C7;
+            text-align: left;
+        }
+        
+        .response_table th, .response_table td {
+            padding-top: 4px;
+            padding-bottom: 4px;
+            border-style: solid;
+            border-width: 1px;
+            border-color: #CBCBCB;
+        }
+        
+        .col1 {
+            width: 350px;
+        }
     </style>
     <script lang="JavaScript">
         function clearForm(f) {
@@ -49,7 +75,7 @@
                 <tr>
                     <td><span class="input_label">Company Name or Symbol:</span></td>
                     <!-- TODO change REQUIRED to use a dialog like example, see what they actually want... -->
-                    <td><input type="text" name="search_input" REQUIRED value="<?php echo isset($_POST["search_input"]) ? $_POST["search_input"] : "" ?>"></td>
+                    <td><input type="text" name="search_input" REQUIRED value="<?php echo isset($_POST['search_input']) ? $_POST['search_input'] : "" ?>"></td>
                 </tr>
                 <tr align="left">
                     <td align="right"></td>
@@ -61,35 +87,48 @@
             </table>
         </form>
     </div>
-<?php
-    // Lookup
-    $lookup_baseurl = 'http://dev.markitondemand.com/MODApis/Api/v2/Lookup/xml?input=';
-    $input = 'GOOG';
-    // TODO would http_get be better?
-    $lookup_response = file_get_contents($lookup_baseurl.$input);
-    $xml_root = new SimpleXMLElement($lookup_response);
-    // TODO Check not empty
-    echo "Got XML response with ",sizeof($xml_root->LookupResult)," entries:<br>";
-    for ($i = 0; $i < sizeof($xml_root->LookupResult); $i++) {
-        echo "Entry $i:<br>";
-        echo $xml_root->LookupResult[$i]->Name,"<br>";
-        echo $xml_root->LookupResult[$i]->Symbol,"<br>";
-        echo $xml_root->LookupResult[$i]->Exchange,"<br><br>";
-    }
-    
-    // Quote
-    $quote_baseurl = 'http://dev.markitondemand.com/MODApis/Api/v2/Quote/json?symbol=';
-    $input = 'AAPL';
-    // TODO would http_get be better?
-    $query_response = file_get_contents($quote_baseurl.$input);
-    echo "Got JSON response:<br>";
-    echo $query_response,"<br>";
-    $json_root = json_decode($query_response);
-    if ($json_root != null && $json_root->{'Status'} == 'SUCCESS') {
-        
-    } else {
-        
-    }
-?>
+    <div id="query_response">
+        <?php
+            if (isset($_POST['search_input'])) {
+                // Lookup
+                $lookup_baseurl = 'http://dev.markitondemand.com/MODApis/Api/v2/Lookup/xml?input=';
+                $input = $_POST['search_input'];
+                $lookup_response = file_get_contents($lookup_baseurl.$input);
+                $xml_root = new SimpleXMLElement($lookup_response);
+                
+                echo "<table class='response_table'>";
+                // Check if there are any matches
+                if (sizeof($xml_root->LookupResult) == 0) {
+                    echo "<tr><td align='center'>No Record has been found</td></tr>";
+                } else {
+                    echo "<tr><th class='col1'>Name</th><th>Symbol</th><th>Exchange</th><th>Details</th></tr>";
+                    for ($i = 0; $i < sizeof($xml_root->LookupResult); $i++) {
+                        echo "<tr>";
+                        echo "<td class='col1'>",$xml_root->LookupResult[$i]->Name,"</td>";
+                        echo "<td>",$xml_root->LookupResult[$i]->Symbol,"</td>";
+                        echo "<td>",$xml_root->LookupResult[$i]->Exchange,"</td>";
+                        echo "<td><a href='","#TODO","'>","More Info</a></td>";
+                        echo "</tr>";
+                    }   
+                }
+                echo "</table>";
+            }
+
+                /*/ Quote
+                $quote_baseurl = 'http://dev.markitondemand.com/MODApis/Api/v2/Quote/json?symbol=';
+                $input = 'AAPL';
+                // TODO would http_get be better?
+                $query_response = file_get_contents($quote_baseurl.$input);
+                echo "Got JSON response:<br>";
+                echo $query_response,"<br>";
+                $json_root = json_decode($query_response);
+                if ($json_root != null && $json_root->{'Status'} == 'SUCCESS') {
+
+                } else {
+
+                }
+            }*/
+        ?>
+    </div>
 <noscript>
 </body>
