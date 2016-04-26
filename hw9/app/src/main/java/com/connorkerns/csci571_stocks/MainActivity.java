b4335ac27f5e96276531449d62c05dd517f7371d
Connor Kerns <connor-k@users.noreply.github.com>
2016-04-25 22:16:43 -0700

    Initial layout for main activity, start to android hw9

---
+++ hw9/app/src/main/java/com/connorkerns/csci571_stocks/MainActivity.java
@@ -0,0 +1,14 @@
+package com.connorkerns.csci571_stocks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("Stock Market Viewer");
+    }
+}
