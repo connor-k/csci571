716f6500606012ed15fb52bfbba69a10bbb9d423
Connor Kerns <connor-k@users.noreply.github.com>
2016-04-29 18:39:43 -0700

    Autocomplete for aapl

---
+++ hw9/app/src/main/java/com/connorkerns/csci571_stocks/LookupElement.java
@@ -0,0 +1,8 @@
+package com.connorkerns.csci571_stocks;
+
+public class LookupElement {
+    public String Symbol;
+    public String Name;
+    public String Exchange;
+    public String Display;
+}
--- hw9/app/src/main/java/com/connorkerns/csci571_stocks/MainActivity.java
+++ hw9/app/src/main/java/com/connorkerns/csci571_stocks/MainActivity.java
@@ -1,7 +1,11 @@
 package com.connorkerns.csci571_stocks;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
@@ -10,6 +14,15 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonParser;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private static String DEBUG_TAG = "MainActivity";
@@ -49,14 +62,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             public void onTextChanged(CharSequence s, int start, int before, int count) {}
         };
         textView.addTextChangedListener(autocompleteWatcher);
+        // From @612, we can just display it on one line
         autocompleteResults = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line);
         //TODO remove sample adds later
-        autocompleteResults.add("Test");
-        autocompleteResults.add("Test2");
-        autocompleteResults.add("Other");
         textView.setAdapter(autocompleteResults);
 
         refreshFavorites();
+        doGetRequest("https://inspired-photon-127022.appspot.com/stock-api.php?input=AAPL");
     }
 
     @Override
@@ -83,17 +95,84 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         textView.setText("");
     }
 
-    int count = 0;
+    private void doGetRequest(String url) {
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            new AutoCompleteRequestTask().execute(url);
+        } else {
+            Log.d(DEBUG_TAG, "Can't do request, no internet connection.");
+        }
+    }
+
+    /**
+     * AsyncTask to do a get request. The code is adapted from
+     * http://developer.android.com/training/basics/network-ops/connecting.html
+     */
+    private class AutoCompleteRequestTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                return downloadUrl(urls[0]);
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Log.d(DEBUG_TAG, "Got autocomplete json=" + result);
+            Gson gson = new Gson();
+            JsonParser parser = new JsonParser();
+            JsonArray array = parser.parse(result).getAsJsonArray();
+            autocompleteResults.clear();
+            for (int i = 0; i < array.size(); ++i) {
+                String item = gson.fromJson(array.get(i).getAsJsonObject().get("Display"),
+                        String.class);
+                Log.d(DEBUG_TAG, "autocomplete result: " + item);
+                autocompleteResults.add(item);
+            }
+        }
+    }
+
+    private String downloadUrl(String myurl) throws IOException {
+        InputStream is = null;
+        try {
+            URL url = new URL(myurl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000);
+            conn.setConnectTimeout(15000);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
+            int response = conn.getResponseCode();
+            Log.d(DEBUG_TAG, "The response is: " + response);
+            is = conn.getInputStream();
+            String contentAsString = "";
+            if (is != null && response == 200) {
+                // Convert the InputStream into a string
+                java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+                contentAsString = s.hasNext() ? s.next() : "";
+                s.close();
+            }
+            return contentAsString;
+
+            // Makes sure that the InputStream is closed after the app is finished using it.
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
+
     /**
      *
      */
     private void doAutoCompleteLookup() {
         Log.d(DEBUG_TAG, "Making lookup request for autocomplete...");
-        if (!textView.getText().toString().isEmpty()) {
-            autocompleteResults.add("Count" + count++);
-        } else {
-            autocompleteResults.clear();
-        }
+
     }
 
     /**
